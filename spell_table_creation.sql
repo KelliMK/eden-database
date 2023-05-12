1a1052fbf5568d75b61b88716a1d5103d06de6e1
KelliMK <kelli.m.knipe@gmail.com>
2023-05-12 19:51:49 -0400

    another master push lol

--- spell_table_creation.sql
+++ spell_table_creation.sql
@@ -2,7 +2,7 @@ CREATE TABLE osfe_spells -- Not sure this should be the final version
 (
 spell_id VARCHAR(25) NOT NULL,
 spell_name VARCHAR(25) NOT NULL,
-spell_cost INT NOT NULL,
+spell_cost INT DEFAULT 0,
 spell_damage INT DEFAULT 0,
 spell_brand ENUM('Anima','Convergence','Doublelift','Glimmer','Hearth','Hexawan','Kinesys','Miseri','Phalanx','Slashfik','None'),
 spell_repeat BIT NOT NULL DEFAULT 0,
@@ -10,6 +10,7 @@ spell_repititions INT DEFAULT 0,
 spell_rarity ENUM('Common','Rare','Epic','Legendary','Calamity','N/A'),
 spell_unlock_level INT, -- Default value should be used in code to indicate inclusion in the expansion
 spell_text VARCHAR(100) NOT NULL,
-spell_flavor VARCHAR(60)
+spell_flavor VARCHAR(60),
+PRIMARY KEY (spell_id)
 )
 ;
